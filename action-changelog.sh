225d365bf152af45287f5939262354148fa3a999
PBK-B <pbk-b@PBK6.cn>
2026-01-14 17:53:20 +0800

    chore(ci): update action-changelog

--- action-changelog.sh
+++ action-changelog.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-current=$(git describe --tags --abbrev=0)
-previous=$(git describe --always --abbrev=0 --tags ${current}^)
+set -euo pipefail
+
+current=${1:-$(git describe --tags --abbrev=0)}
+previous=$(git describe --tags --match '[0-9]*' --abbrev=0 "${current}^" 2>/dev/null || git rev-parse "${current}^")
+
+echo "![GitHub Downloads](https://img.shields.io/github/downloads/PBK-B/squirrel/${current}/total)"
+echo ""
 
 echo "**Change log since ${previous}:**"
 
-git log --oneline --decorate ${previous}...${current} --pretty="format:- %h %s" | grep -v Merge
+git log --no-merges "${previous}..${current}" --pretty="tformat:- %h %s"
